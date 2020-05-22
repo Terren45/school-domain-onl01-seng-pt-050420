09fab25d1923013ea00697b902bd0d9f8c5ae40d
Terren Culberson <culbersonterren@yahoo.com>
2020-05-22 01:42:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+  attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+ end
+ 
+ def add_student(student_name, grade)
+    @roster[grade] = [] if @roster[grade] == nil
+   @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+ end
